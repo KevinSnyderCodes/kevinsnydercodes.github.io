7ca645833b9bcba1daf9a380d88379d3c578fbe0
Kevin Snyder <kevinsnyder@Kevins-Raft-MacBook-Pro.local>
2024-01-12 13:15:16 -0800

    Add missing `key` prop

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -12,8 +12,8 @@ const Blog: React.FC = () => {
       <div className="w-full h-screen bg-zinc-700 text-white">
         <div className="container mx-auto h-full">
           <div className="flex flex-col w-full h-full p-24">
-            {posts.map((props) => (
-              <BlogItem {...props} />
+            {posts.map((props, i) => (
+              <BlogItem key={i} {...props} />
             ))}
           </div>
         </div>
