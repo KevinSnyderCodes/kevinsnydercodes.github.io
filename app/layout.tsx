f35452609cb3198f00bae9dfc673b0e000efa636
Kevin Snyder <kevinsnyder@Kevins-Raft-MacBook-Pro.local>
2024-08-01 12:58:42 -0700

    Remove header, some unused variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@
 
 import React, { useCallback, useState } from "react";
 
-import type { Metadata } from "next";
-
 import { Inter } from "next/font/google";
 
 import "./globals.css";
@@ -33,40 +31,14 @@ const MenuItem: React.FC<MenuItemProps> = ({ title, href, onClick }) => {
   );
 };
 
-const menuItems: Omit<MenuItemProps, "onClick">[] = [
-  {
-    title: "Home",
-    href: "/",
-  },
-  {
-    title: "About",
-    href: "/about",
-  },
-  {
-    title: "Blog",
-    href: "/blog",
-  },
-];
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const handleClick = useCallback(() => {
-    setIsMenuOpen((prevState) => !prevState);
-  }, []);
-
-  const onMenuItemClick = useCallback(() => {
-    setIsMenuOpen(false);
-  }, []);
-
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
         {children}
         <Footer />
       </body>
